7cf7bb68828a27c17583e4da25f1eaa0113d6306
Tonny <tonnymakondesa6@gmail.com>
2022-11-26 11:25:46 +0200

    footer added

---
+++ src/footer.js
@@ -0,0 +1,22 @@
+const Footer = () => {
+    return ( 
+        <div>
+            <footer id="about">
+                <h2 class="h2">About</h2>
+                <ul>
+                    <a href="http://" target="_blank" rel="noopener noreferrer"></a>
+                    <li class="footer"><a href="http://www.discord.com/tonny2wo6ix5ive" target="_blank" rel="noopener noreferrer">Discord</a></li>
+                    <li class="footer"><a href="http://www.facebook.com/tonny-makondesa" target="_blank" rel="noopener noreferrer">FaceBook</a></li>
+                    <li class="footer"><a href="http://www.instagram.com/tonny2wo6ix5ive" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+                    <li class="footer"><a href="http://www.linkedin.com/in/tonny-makondesa" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+                    <li class="footer"><a href="http://www.twitter.com/tonny2wo6ix5ive" target="_blank" rel="noopener noreferrer">Twitter</a></li>
+                </ul>
+                <div class="foot">
+                    <p class="copy">&copy;Tonny Makondesa 2022</p>
+                </div>
+            </footer>
+        </div>
+     );
+}
+ 
+export default Footer;
