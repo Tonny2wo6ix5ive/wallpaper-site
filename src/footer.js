019a24ed4f30c5b5d074e9137b7120e3d1365495
Tonny <tonnymakondesa6@gmail.com>
2022-11-27 16:36:23 +0200

    update footer

--- src/footer.js
+++ src/footer.js
@@ -1,4 +1,7 @@
 const Footer = () => {
+
+    const year = new Date;
+
     return ( 
         <footer id="about">
             <h2 class="h2">About</h2>
@@ -11,7 +14,7 @@ const Footer = () => {
                 <li class="footer"><a href="http://www.twitter.com/tonny2wo6ix5ive" target="_blank" rel="noopener noreferrer">Twitter</a></li>
             </ul>
             <div class="foot">
-                <p class="copy">&copy;Tonny Makondesa 2022</p>
+                <p class="copy">&copy;Tonny Makondesa {year.getFullYear()}</p>
             </div>
         </footer>
      );
